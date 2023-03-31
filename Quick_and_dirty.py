398cdd33b37c5efab86bbbf164b74c7dffad77c7
Dkeller08 <danielkellerjazz@gmail.com>
2023-03-31 11:20:41 +0200

    Torsion

--- Quick_and_dirty.py
+++ Quick_and_dirty.py
@@ -11,20 +11,20 @@ def angle(overlap, non_overlap, thickness):
     return thickness / (overlap + non_overlap)
 
 
-overlap_start = 5 * 10 ** -9
-overlap_stop = 30 * 10 ** -9
-thickness = 25 * 10 ** -9
-non_overlap_start = 5 * 10 ** -9
-non_overlap_stop = 20 * 10 ** -9
-overlap_range = np.arange(overlap_start, overlap_stop, 5 * 10 ** -9)
-non_overlap_range = np.arange(non_overlap_start, non_overlap_stop, 5 * 10 ** -9)
-voltage = 80
+overlap_start = 5 * 10 ** -6
+overlap_stop = 30 * 10 ** -6
+thickness = 25 * 10 ** -6
+non_overlap_start = 5 * 10 ** -6
+non_overlap_stop = 20 * 10 ** -6
+overlap_range = np.arange(overlap_start, overlap_stop, 5 * 10 ** -6)
+non_overlap_range = np.arange(non_overlap_start, non_overlap_stop, 5 * 10 ** -6)
+voltage = 40
 
 t = True
 
 for i in overlap_range:
     for j in non_overlap_range:
-        T = ((voltage ** 2) * dC_dAngle(i+j, thickness, j, 0.75, 1000, 3.5*10**-9)) / 2
+        T = ((voltage ** 2) * dC_dAngle(i+j, thickness, j, 0.75, 1000, 3.5*10**-6)) / 2
         angles = np.linspace(0, 0.75, 999)
         plt.plot(angles, T, label=f'{i} {j}')
         plt.pause(0.1)

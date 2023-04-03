445c58642392dee1bdc201eb95072c71ce9b7d72
Dkeller08 <danielkellerjazz@gmail.com>
2023-04-03 19:26:12 +0200

    Correct mistake in quicky_dirty and make plots nicer

--- Quick_and_dirty.py
+++ Quick_and_dirty.py
@@ -13,21 +13,23 @@ def angle(overlap, non_overlap, thickness):
 
 
 overlap_start = 50 * 10 ** -6
-overlap_stop = 150 * 10 ** -6
+overlap_stop = 100 * 10 ** -6
 thickness = 25 * 10 ** -6
 non_overlap_start = 5 * 10 ** -6
 non_overlap_stop = 20 * 10 ** -6
 overlap_range = np.arange(overlap_start, overlap_stop, 5 * 10 ** -6)
 non_overlap_range = np.arange(non_overlap_start, non_overlap_stop, 5 * 10 ** -6)
 voltage = 40
-j = 35 * 10 ** -6  # final non-overlapping length
+j = 25 * 10 ** -6  # final non-overlapping length
 t = True
 
 for i in overlap_range:
-    T = 250*((voltage ** 2) * dC_dAngle(i + j, thickness, j, 0.75, 1000, 3.5 * 10 ** -6)) / 2
+    T = ((voltage ** 2) * dC_dAngle(i + j, thickness, j, 0.75, 1000, 3.5 * 10 ** -6))
     angles = np.linspace(0, 0.75, 999)
-    plt.axhline(y=-4.857500543980081e-09, color='r', linestyle='-')
-    plt.plot(angles, T, label=f'{i} {j}')
+    # plt.axhline(y=-4.857500543980081e-09, color='r', linestyle='-')
+    plt.plot(angles, T, label=f'overlapping length: {i} m')
     plt.pause(0.1)
+plt.xlabel("angle (rad)")
+plt.ylabel("Moment (Nm)")
 plt.legend()
 plt.show()
--- perfect_spring.py
+++ perfect_spring.py
@@ -1,6 +1,6 @@
 youngs_E = 160 * 10 ** 9  # Pa
 poisson = 0.22
-L1 = 600 * 10 ** -6
+L1 = 300 * 10 ** -6
 h1 = 25 * 10 ** -6
 b1 = 30 * 10 ** -6
 I1 = (b1 * h1 ** 3) / 12
@@ -9,6 +9,6 @@ h2 = 3.5 * 10 ** -6
 b2 = 25 * 10 ** -6
 I2 = (b2 * h2 ** 3) / 12
 n = 1
-k_3 = 1 / ((L1 / (8 * I1 * youngs_E)) + (L2 / (n * I2 * youngs_E))) # Nm/rad
+k_3 = 1 / ((L1 / (8 * I1 * youngs_E)) + (L2 / (n * I2 * youngs_E)))  # Nm/rad
 moment_needed = k_3 * 0.17
-print(moment_needed)
+print(k_3)

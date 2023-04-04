7b7abdb3adbfe02c9a82aff7db6ee8d27634a87d
Dkeller08 <danielkellerjazz@gmail.com>
2023-04-04 17:49:29 +0200

    9iojsd

--- Quick_and_dirty.py
+++ Quick_and_dirty.py
@@ -12,21 +12,22 @@ def angle(overlap, non_overlap, thickness):
     return thickness / (overlap + non_overlap)
 
 
-overlap_start = 50 * 10 ** -6
-overlap_stop = 100 * 10 ** -6
+overlap_start = 40 * 10 ** -6
+overlap_stop = 80 * 10 ** -6
 thickness = 25 * 10 ** -6
 non_overlap_start = 5 * 10 ** -6
 non_overlap_stop = 20 * 10 ** -6
 overlap_range = np.arange(overlap_start, overlap_stop, 5 * 10 ** -6)
 non_overlap_range = np.arange(non_overlap_start, non_overlap_stop, 5 * 10 ** -6)
 voltage = 40
-j = 25 * 10 ** -6  # final non-overlapping length
+j = 68.75 * 10 ** -6  # final non-overlapping length
 t = True
 
 for i in overlap_range:
-    T = ((voltage ** 2) * dC_dAngle(i + j, thickness, j, 0.75, 1000, 3.5 * 10 ** -6))
+    T = -160*((voltage ** 2) * dC_dAngle(i + j, thickness, j, 0.75, 1000, 3.5 * 10 ** -6))
     angles = np.linspace(0, 0.75, 999)
-    # plt.axhline(y=-4.857500543980081e-09, color='r', linestyle='-')
+    plt.axhline(y=4*10**-9, color='r', linestyle='-')
+    plt.axvline(x=0.19, color='r', linestyle='-')
     plt.plot(angles, T, label=f'overlapping length: {i} m')
     plt.pause(0.1)
 plt.xlabel("angle (rad)")
--- perfect_spring.py
+++ perfect_spring.py
@@ -1,14 +1,22 @@
 youngs_E = 160 * 10 ** 9  # Pa
 poisson = 0.22
-L1 = 300 * 10 ** -6
+shear_G = youngs_E / (2 * (1 + poisson))  # Pa
+# Grote meneer
+L1 = 500 * 10 ** -6 #halve lengte beam
 h1 = 25 * 10 ** -6
-b1 = 30 * 10 ** -6
+b1 = 3.5 * 10 ** -6
 I1 = (b1 * h1 ** 3) / 12
+k1 = (3 * I1 * youngs_E)/L1
+
+# Kleine meneer
 L2 = 500 * 10 ** -6
-h2 = 3.5 * 10 ** -6
-b2 = 25 * 10 ** -6
+h2 = 25 * 10 ** -6
+b2 = 3.5 * 10 ** -6
+J2 = (1 / 16) * h2 * (b2 ** 3) * ((16 / 3) - 3.36 * (b2 / h2) * (1 - ((b2 ** 4) / 12 * h2 ** 4)))
 I2 = (b2 * h2 ** 3) / 12
+k_veertje = shear_G * J2 / L2
 n = 1
-k_3 = 1 / ((L1 / (8 * I1 * youngs_E)) + (L2 / (n * I2 * youngs_E)))  # Nm/rad
+#k_3 = 4 / ((1/k1)+(1/(2*k_veertje)))  # Nm/rad
+k_3 = 4*k1
 moment_needed = k_3 * 0.17
 print(k_3)

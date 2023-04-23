ddcb8dc4406c50a52d6e6cb20cf1bc51d9ef2ff1
jasperverduijn <90001498+jasperverduijn@users.noreply.github.com>
2023-04-23 16:37:37 +0200

    Correct values

--- Oscillator.py
+++ Oscillator.py
@@ -13,9 +13,10 @@ freq = 474
 
 def theta_dd(theta,t):
     M = -(1e-10*140)*np.sin(2*np.pi*freq*t)
-    C = 1e-9
     K = 4.28e-8 #Nm/rad
     I = 4.8101e-15 
+    C = 2*0.95*np.sqrt(K*I)
+
     return [theta[1], (M-C*theta[1]-K*theta[0])/I]
 
 
@@ -26,17 +27,16 @@ step = (1/freq)/50
 theta_0 = 0.0035
 
 time = np.arange(t_start, t_end, step)
-#Moment = 0.001*np.sin(2*np.pi*freq*time)
 
 theta, theta_d = odeint(theta_dd, [theta_0, 0], time).T   
 angle = theta*180/np.pi
 
-plt.figure(1)
-plt.plot(time, theta, 'orange', linewidth = 2)
-plt.xlabel('time (s)')
-plt.ylabel('theta (rad)')
-plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
-plt.grid(True)
+#plt.figure(1)
+#plt.plot(time, theta, 'orange', linewidth = 2)
+#plt.xlabel('time (s)')
+#plt.ylabel('theta (rad)')
+#plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+#plt.grid(True)
 
 plt.figure(2)
 plt.plot(time, angle, 'orange', linewidth = 2)

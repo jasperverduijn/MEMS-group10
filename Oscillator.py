7c43f3256921cc9c197a9f2360607cf9d1e95a08
jasperverduijn <90001498+jasperverduijn@users.noreply.github.com>
2023-04-23 16:32:35 +0200

    Create Oscillator.py

---
+++ Oscillator.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Apr 23 15:45:23 2023
+
+@author: Jasper Verduijn
+"""
+
+from matplotlib import pyplot as plt
+from scipy.integrate import odeint
+import numpy as np
+
+freq = 474
+
+def theta_dd(theta,t):
+    M = -(1e-10*140)*np.sin(2*np.pi*freq*t)
+    C = 1e-9
+    K = 4.28e-8 #Nm/rad
+    I = 4.8101e-15 
+    return [theta[1], (M-C*theta[1]-K*theta[0])/I]
+
+
+Cycles= 10
+t_start = 0
+t_end = Cycles*(1/freq)
+step = (1/freq)/50
+theta_0 = 0.0035
+
+time = np.arange(t_start, t_end, step)
+#Moment = 0.001*np.sin(2*np.pi*freq*time)
+
+theta, theta_d = odeint(theta_dd, [theta_0, 0], time).T   
+angle = theta*180/np.pi
+
+plt.figure(1)
+plt.plot(time, theta, 'orange', linewidth = 2)
+plt.xlabel('time (s)')
+plt.ylabel('theta (rad)')
+plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+plt.grid(True)
+
+plt.figure(2)
+plt.plot(time, angle, 'orange', linewidth = 2)
+plt.xlabel('time (s)')
+plt.ylabel('angle (deg)')
+plt.ticklabel_format(style='sci', axis='x', scilimits=(0,0))
+plt.grid(True)
